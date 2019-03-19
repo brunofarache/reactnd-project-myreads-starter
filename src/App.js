bb9be1d29882c93830b4a07492229c9b92c7133c
Bruno Farache <bruno.farache@liferay.com>
2019-03-19 16:57:45 -0300

    fetching books from server

--- src/App.js
+++ src/App.js
@@ -13,92 +13,44 @@ class BooksApp extends React.Component {
 		 * pages, as well as provide a good URL they can bookmark and share.
 		 */
 		showSearchPage: false,
-		shelves: [
-			{
+		shelves: {
+			'currentlyReading': {
 				title: 'Currently Reading',
-				id: 'currentlyReading',
-				books: [
-					{
-						id: '1',
-						title: 'To Kill a Mockingbird',
-						authors: ['Harper Lee', 'oi'],
-						imageLinks: {
-							thumbnail: 'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
-						}
-					},
-					{
-						id: '2',
-						title: "Ender's Game",
-						authors: ['Orson Scott Card'],
-						imageLinks: {
-							thumbnail: 'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'
-						}
-					}
-				]
-			}
-		,
-			{
+				books: []
+			},
+			'wantToRead': {
 				title: 'Want to Read',
-				id: 'wantToRead',
-				books: [
-					{
-						id: '3',
-						title: '1776',
-						authors: ['David McCullough'],
-						imageLinks: {
-							thumbnail: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api'
-						}
-					},
-					{
-						id: '4',
-						title: "Harry Potter and the Sorcerer's Stone",
-						authors: ['J.K. Rowling'],
-						imageLinks: {
-							thumbnail: 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api'
-						}
-					}
-				]
-			}
-		,
-			{
+				books: []
+			},
+			'read': {
 				title: 'Read',
-				id: 'read',
-				books: [
-					{
-						id: '5',
-						title: 'The Hobbit',
-						authors: ['J.R.R. Tolkien'],
-						imageLinks: {
-							thumbnail: 'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api'
-						}
-					},
-					{
-						id: '6',
-						title: "Oh, the Places You'll Go!",
-						authors: ['Seuss'],
-						imageLinks: {
-							thumbnail: 'http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api'
-						}
-					},
-					{
-						id: '7',
-						title: 'The Adventures of Tom Sawyer',
-						authors: ['Mark Twain'],
-						imageLinks: {
-							thumbnail: 'http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api'
-						}
-					}
-				]
+				books: []
 			}
-		]
+		}
+	}
+
+	componentDidMount() {
+		const { shelves } = this.state;
+
+		BooksAPI.getAll().then((books) => {
+			books.forEach(book => {
+				const {id, title, authors, imageLinks} = book;
+
+				shelves[book.shelf].books.push({
+					id,
+					title,
+					authors,
+					imageLinks
+				});
+			});
+
+			this.setState(shelves);
+		});
 	}
 
 	render() {
 		const { shelves } = this.state;
-
-		BooksAPI.getAll().then((books) => {
-			console.log(books);
-		});
+		const { currentlyReading, wantToRead, read} = shelves;
 
 		return (
 			<div className="app">
@@ -130,17 +82,35 @@ class BooksApp extends React.Component {
 						</div>
 						<div className="list-books-content">
 							<div>
-								{shelves.map(shelf => (
-									<BookShelf key={shelf.id} title={shelf.title}>
-										{shelf.books.map(book => (
-											<Book
-												key={book.id}
-												title={book.title}
-												authors={book.authors}
-												thumbnail={book.imageLinks.thumbnail} />		
-										))}
-									</BookShelf>
-								))}
+								<BookShelf key={currentlyReading.id} title={currentlyReading.title}>
+									{currentlyReading.books.map(book => (
+										<Book
+											key={book.id}
+											title={book.title}
+											authors={book.authors}
+											thumbnail={book.imageLinks.thumbnail} />		
+									))}
+								</BookShelf>
+
+								<BookShelf key={wantToRead.id} title={wantToRead.title}>
+									{wantToRead.books.map(book => (
+										<Book
+											key={book.id}
+											title={book.title}
+											authors={book.authors}
+											thumbnail={book.imageLinks.thumbnail} />		
+									))}
+								</BookShelf>
+
+								<BookShelf key={read.id} title={read.title}>
+									{read.books.map(book => (
+										<Book
+											key={book.id}
+											title={book.title}
+											authors={book.authors}
+											thumbnail={book.imageLinks.thumbnail} />		
+									))}
+								</BookShelf>
 							</div>
 						</div>
 						<div className="open-search">
