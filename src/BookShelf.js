edcd766b75083e25f7a31dee3d02c8bc3ead8c9f
Bruno Farache <bruno.farache@liferay.com>
2019-03-19 11:31:49 -0300

    BookShelf component

---
+++ src/BookShelf.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+// TODO: missing <li> for each Book
+class BookShelf extends React.Component {
+    render() {
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">{this.props.children}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// TODO: add children type to array of Books
+BookShelf.propTypes = {
+    title: PropTypes.string.isRequired
+};
+
+export default BookShelf;
