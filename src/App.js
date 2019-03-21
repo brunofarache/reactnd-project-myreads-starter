6193334d5ea640f9906e1e955dc882e08e4e1ae7
Bruno Farache <bruno.farache@liferay.com>
2019-03-21 00:04:42 -0300

    remove comments

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import BookShelf from './BookShelf';
 
 class BooksApp extends React.Component {
 	state = {
-		/**
-		 * TODO: Instead of using this state variable to keep track of which page
-		 * we're on, use the URL in the browser's address bar. This will ensure that
-		 * users can use the browser's back and forward buttons to navigate between
-		 * pages, as well as provide a good URL they can bookmark and share.
-		 */
 		showSearchPage: false,
 		books: [],
 		searchedBooks: []
@@ -106,14 +100,6 @@ class BooksApp extends React.Component {
 						<div className="search-books-bar">
 							<button className="close-search" onClick={() => this.setState({ showSearchPage: false, searchedBooks: [] })}>Close</button>
 							<div className="search-books-input-wrapper">
-								{/*
-									NOTES: The search from BooksAPI is limited to a particular set of search terms.
-									You can find these search terms here:
-									https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-									However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-									you don't find a specific author or title. Every search is limited by search terms.
-								*/}
 								<input type="text" placeholder="Search by title or author" onChange={(event) => this.onSearch(event.target.value)} />
 
 							</div>
